ec4896ffd2351dc04f76b31493c9cb9808e61443
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-02-12 09:10:56 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_reader :title
+    attr_accessor :author :count
+
+    def initialize(title)
+        @title = title
+    end
+    def turn_page()
+        puts "The World According to Garp"
+    end
+end
 
 
